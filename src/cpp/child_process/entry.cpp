325c50790d42410f268655e5c2df3a3e0459dd18
X.Y.Z <g.for.xyz@gmail.com>
2022-05-24 22:34:58 +0800

    feat(child_process): 在 setup 时解析 json; 增加调试输出项; 转化到 argv

--- src/cpp/child_process/entry.cpp
+++ src/cpp/child_process/entry.cpp
@@ -3,19 +3,21 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using nlohmann::json;
-using std::cout;
-using std::endl;
+using std::cout, std::endl;
 using std::ifstream;
 using std::string;
+using std::vector;
 
 ChildProcessArgs *args = nullptr;
 
 void _setup_args(const char *config_path, const char *init_lock_path,
                  const char *sock_path, int pty_socket) {
     args = new ChildProcessArgs;
-    args->config_path = config_path;
+
+    ifstream(config_path) >> args->config;
     args->init_lock_path = init_lock_path;
     args->sock_path = sock_path;
     args->pty_socket = pty_socket;
@@ -24,13 +26,54 @@ void _setup_args(const char *config_path, const char *init_lock_path,
 int _child_main() {
     assert(args != nullptr);
 
-    json config;
-    ifstream(args->config_path) >> config;
+    auto rootfs = args->config["root"]["path"].get<string>();
+    auto process_cwd = args->config["process"]["cwd"].get<string>();
 
-    auto rootfs = config["root"]["path"].get<string>();
+    auto process_env = args->config["process"]["env"];
+    assert(process_env.is_array());
 
-    cout << "容器的 rootfs 是: " << rootfs << endl;
-    cout << "容器的 process.args 是: "
-         << config["process"]["args"][0].get<string>() << endl;
+    auto process_args = args->config["process"]["args"];
+    assert(process_args.is_array());
+
+    auto process_command = process_args[0].get<string>();
+    auto process_argv = args_to_argv(process_args);
+    auto process_argv_len = process_args.size();
+
+    cout << "rootfs             = " << rootfs << endl;
+    cout << "process_cwd        = " << process_cwd << endl;
+    cout << "process_env        = " << process_env << endl;
+    cout << "process_command    = " << process_command << endl;
+
+    for (size_t i = 0; i < process_argv_len; i++) {
+        cout << "process_argv[" << i << "]    = " << process_argv[i] << endl;
+    }
+
+    // 实际上子进程将被替换, 所以不必 return 0, 也不必 delete 什么.
+    // 仅为了避免 linter 的警告
     return 0;
+}
+
+char **args_to_argv(json &args) {
+    // ref: https://linux.die.net/man/3/execvp
+    //
+    // int execvp(const char *file, char *const argv[]);
+    //
+    // The execv(), execvp(), and execvpe() functions provide an array of
+    // pointers to null-terminated strings that represent the argument list
+    // available to the new program. The first argument, by convention, should
+    // point to the filename associated with the file being executed. The array
+    // of pointers must be terminated by a NULL pointer.
+
+    char **result = new char *[args.size() + 1];
+
+    for (size_t i = 0; i < args.size(); i++) {
+        auto arg = args[i].get<string>();
+
+        result[i] = new char[arg.size() + 1];
+        std::copy(arg.begin(), arg.end(), result[i]);
+        result[i][arg.size()] = '\0';
+    }
+    result[args.size()] = nullptr;
+
+    return result;
 }
--- src/cpp/child_process/entry.hpp
+++ src/cpp/child_process/entry.hpp
@@ -2,9 +2,12 @@
 #define _CHILD_PROCESS_ENTRY_HPP
 
 #include "exports.hpp"
+#include "json.hpp"
+
+using nlohmann::json;
 
 struct ChildProcessArgs {
-    const char *config_path;
+    json config;
     const char *init_lock_path;
     const char *sock_path;
     int pty_socket;
@@ -13,13 +16,14 @@ struct ChildProcessArgs {
 void _setup_args(const char *config_path, const char *init_lock_path,
                  const char *sock_path, int pty_socket);
 int _child_main();
+char **args_to_argv(json &args);
 
 extern "C" {
 void setup_args(const char *config_path, const char *init_lock_path,
                 const char *sock_path, int pty_socket) {
     _setup_args(config_path, init_lock_path, sock_path, pty_socket);
-}
-int child_main() { return _child_main(); }
+    }
+    int child_main() { return _child_main(); }
 }
 
 #endif
