d7ac5fadb97b972642b9c58d0fda79d21303160b
Fuu <g.for.xyz@gmail.com>
2022-06-01 13:49:56 +0800

    refactor(child_process): 将 extern C 函数的实现移动到 entry.cpp

--- src/cpp/child_process/entry.hpp
+++ src/cpp/child_process/entry.hpp
@@ -9,12 +9,4 @@ void _setup_args(const char *config_path, const char *init_lock_path,
 int _child_main();
 char **args_to_argv(nlohmann::json &args);
 
-extern "C" {
-void setup_args(const char *config_path, const char *init_lock_path,
-                const char *sock_path, int pty_socket) {
-    _setup_args(config_path, init_lock_path, sock_path, pty_socket);
-}
-int child_main() { return _child_main(); }
-}
-
 #endif
